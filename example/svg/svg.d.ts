ec886c59df0d491f143b01671b0459e801ca056f
sinjayxie <sinjayxie@gmail.com>
2026-06-23 10:35:28 +0800

    update:) Shared manualChunks

---
+++ example/svg/svg.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Svg Icon names (vitejs-svg-plugin)
+ * @author SinJay Xie
+ * @date 4/13/2026, 3:55:50 PM
+ * @description Load SVG amount: 2
+ */
+export type SvgIconNames = 'add' | 'add2' | string;
